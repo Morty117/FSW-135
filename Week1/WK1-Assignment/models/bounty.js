65b3c8385e8fdd83bd3d903914c68f33d960c8f8
antho <anthony.hernandez1@bryanuniversity.edu>
2021-12-28 09:37:58 -0700

    WK1-Assingment

--- Week1/WK1-Assignment/models/bounty.js
+++ Week1/WK1-Assignment/models/bounty.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 // bounty Schema
-const bountySchema = newSchema({
+const bountySchema = Schema({
     first: {
         type: String,
         required: true
--- Week1/WK1-Assignment/server.js
+++ Week1/WK1-Assignment/server.js
@@ -10,17 +10,28 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 // Connect to DB
-mongoose.connect('mongodb://localhost:27017/bountiesdb',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    },
-    () => console.log("Connected to the DB")
-)
+// mongoose.connect('mongodb://localhost:27017/bountiesdb',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false
+//     },
+//     () => console.log("Connected to the DB")
+// )
+
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/bountiesdb');
+    console.log("Connected to the DB")
+}
 
 app.use('/bounties', bountyHunterRouter)
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({ errMsg: err.message })
+})
 
 app.listen(PORT, () => {
     console.log(`App started on port: ${PORT}`)
